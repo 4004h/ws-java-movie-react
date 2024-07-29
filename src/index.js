b871116f3463c1129ee9d13555fd38e7f000a67e
4004h <minjihwan9500@gmail.com>
2024-07-29 14:39:59 +0900

    st-5 어바웃 스타일

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './st-5/App';
+import App from './st-6/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
